4ae267717568d07d520cf67260eaea3bf1cff69b
Simon McFarlane <simon@piston.pw>
2015-10-24 12:50:05 -0700

    Add puts color support

--- arch/x86.c
+++ arch/x86.c
@@ -10,16 +10,20 @@ void screen_clear() {
 	char *vidptr = vidmem;
 	for (int i = 0; i < _X86_SCREEN_COLS * _X86_SCREEN_ROWS * 2; i+=2) { // two-byte stride
 		vidptr[i] = ' ';
-		vidptr[i + 1] = _X86_SCREEN_GRAYBLACK;
+		vidptr[i + 1] =	COLOR_LIGHT_GREY | COLOR_BLACK << 4;
 	}
 }
 
 void puts(const char *s) {
+	puts_color(s, COLOR_LIGHT_GREY, COLOR_BLACK);
+}
+
+void puts_color(const char *s, x86_colors fg, x86_colors bg) {
 	char *vidptr = vidmem;
 	unsigned int i = 0;
 	for (char* t = (char*) s; *t; t++) {
 		vidptr[i] = *t;
-		vidptr[i + 1] = _X86_SCREEN_GRAYBLACK;
+		vidptr[i + 1] = fg | bg << 4;
 		i += 2;
 	}
 }
--- arch/x86.h
+++ arch/x86.h
@@ -5,6 +5,26 @@
 #define _X86_SCREEN_COLS 80
 #define _X86_SCREEN_GRAYBLACK 0x07
 
+typedef enum {
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4,
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+	COLOR_DARK_GREY = 8,
+	COLOR_LIGHT_BLUE = 9,
+	COLOR_LIGHT_GREEN = 10,
+	COLOR_LIGHT_CYAN = 11,
+	COLOR_LIGHT_RED = 12,
+	COLOR_LIGHT_MAGENTA = 13,
+	COLOR_LIGHT_BROWN = 14,
+	COLOR_WHITE = 15,
+} x86_colors;
+
 void arch_init();
 void screen_clear();
 void puts(const char* s);
+void puts_color(const char *s, x86_colors fg, x86_colors bg);
