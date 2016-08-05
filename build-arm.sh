6108d718a8d5cb238a2126d8c43a39a3b5bd37ed
Ian Kronquist <iankronquist@gmail.com>
2016-08-04 20:31:32 -0400

    Add arm build script

---
+++ build-arm.sh
@@ -0,0 +1,11 @@
+set -e
+cd "$(dirname "$0")"
+CFLAGS="-c -mcpu=arm1176jzf-s -fpic -ffreestanding -std=c11  -O2 -Wall -Wextra -Werror -I . -I dlibc -g"
+
+mkdir -p build
+arm-none-eabi-gcc $CFLAGS boot/arm/boot.S -o build/boot.o
+arm-none-eabi-gcc $CFLAGS core/kernel.c -o build/kernel.o
+arm-none-eabi-gcc $CFLAGS arch/arm/uart.c -o build/uart.o
+arm-none-eabi-gcc -T ./arch/arm/link.ld -o build/dk.elf -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o build/uart.o
+
+# Run with qemu-system-arm -kernel build/dk.elf -no-reboot  -serial stdio -M raspi2
